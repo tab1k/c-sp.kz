74fb1df20e115a992f314ba4917235290a0d76de
Tabigat Karbayev <tab1k@MacBook-Pro-Tabigat.local>
2025-03-11 00:00:31 +0500

    Debug: True

--- src/website/settings.py
+++ src/website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--w$616^)l@ya-44ey*!6od&v43n-t-2ui9gn*)*pq=a7_ksl8^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*", "127.0.0.1", "localhost"]
 
