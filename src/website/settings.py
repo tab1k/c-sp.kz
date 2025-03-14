0b5038cbf13c07e4880a33244ba5753e894180d8
Tabigat Karbayev <tab1k@MacBook-Pro-Tabigat.local>
2025-03-14 23:43:38 +0500

    added services

--- src/website/settings.py
+++ src/website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--w$616^)l@ya-44ey*!6od&v43n-t-2ui9gn*)*pq=a7_ksl8^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", "127.0.0.1", "localhost"]
 
@@ -75,23 +75,23 @@ WSGI_APPLICATION = "website.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'csp_db',
-#         'USER': 'tab1k',
-#         'PASSWORD': 'TOBI8585',
-#         'HOST': 'postgres',  # Должно совпадать с именем контейнера
-#         'PORT': '5432',
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'csp_db',
+        'USER': 'tab1k',
+        'PASSWORD': 'TOBI8585',
+        'HOST': 'postgres',  # Должно совпадать с именем контейнера
+        'PORT': '5432',
+    }
+}
 
 
 
