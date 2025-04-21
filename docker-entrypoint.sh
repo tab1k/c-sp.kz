21275afefaa04e9fbbcf000bfe85f62b0338e656
Tabigat Karbayev <tab1k@MacBook-Pro-Tabigat.local>
2025-04-21 21:58:57 +0500

    adddeedd

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# Останавливаем выполнение при ошибке
 set -e
 
-# Ожидаем, пока PostgreSQL не запустится
 echo "Ожидание PostgreSQL..."
 until pg_isready -h postgres -p 5432; do
   sleep 1
@@ -10,9 +8,11 @@ done
 
 echo "PostgreSQL доступен, выполняем миграции..."
 python src/manage.py migrate
-
 python src/manage.py collectstatic --noinput
 
-# Запускаем сервер Django
-echo "Запускаем Django-сервер..."
-exec python src/manage.py runserver 0.0.0.0:8000
+echo "Запускаем Gunicorn-сервер..."
+exec gunicorn website.wsgi:application \
+  --bind 0.0.0.0:8000 \
+  --workers 3 \
+  --threads 2 \
+  --timeout 120
